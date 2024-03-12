d64e79f22c9b5fdc1c6b9791fb07c757c820866f
01fe22bec267 <120108239+01fe22bec267@users.noreply.github.com>
2024-03-12 20:45:03 +0530

    Add files via upload

---
+++ 3and5.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+main()
+{
+ int a;
+ printf("enter number\n");
+ scanf("%d",&a);
+ if(a>=0&&a%2==0)
+ {
+   printf("HAPPY NEW YEAR");
+
+ }
+else
+{
+printf("WELCOME TO 2023");
+}
+
+}
